a334fe12765465ed0266a12e4b6601682a95e74a
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-13 23:04:37 +0500

    submarine game

---
+++ src/components/submarine.js
@@ -0,0 +1,29 @@
+// src/Submarine.js
+import React from 'react';
+import './submarine.css';
+
+const Submarine = () => {
+  return (
+    <div className="sea">
+        <div className="circle-wrapper">
+            <div className="bubble"></div>
+            <div className="submarine-wrapper">
+                <div className="submarine-body">
+                    <div className="window"></div>
+                    <div className="engine"></div>
+                    <div className="light"></div>
+                </div>
+                <div className="helix"></div>
+                <div className="hat">
+                  <div className="leds-wrapper">
+                      <div className="periscope"></div>
+                      <div className="leds"></div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Submarine;
