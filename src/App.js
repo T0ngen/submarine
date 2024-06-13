6735e152fa341ea8490b317dc725ebd1d8f808ae
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-13 23:47:40 +0500

    -UseEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import Submarine from './components/submarine';
 
 
 
 function App() {
-  useEffect(() => {
-    if (window.Telegram) {
-      const tg = window.Telegram.WebApp;
+  
+  if (window.Telegram) {
+    const tg = window.Telegram.WebApp;
 
-      tg.expand();
-      tg.isClosingConfirmationEnabled = true;
+    tg.expand();
+    tg.isClosingConfirmationEnabled = true;
 
-      
-    }
-  }, []);
+    
+  }
+  
   return (
     <div className="App">
       <Submarine />
