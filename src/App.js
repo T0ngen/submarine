ec1be7ae76452daed2f035c5d0567224e18a09ba
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-13 23:41:24 +0500

    v.005 tgexpand

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Submarine from './components/submarine';
 
 
 
 function App() {
+  useEffect(() => {
+    if (window.Telegram) {
+      const tg = window.Telegram.WebApp;
+
+      tg.expand();
+      tg.isClosingConfirmationEnabled = true;
+
+      
+    }
+  }, []);
   return (
     <div className="App">
       <Submarine />
