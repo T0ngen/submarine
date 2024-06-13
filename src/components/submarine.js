b2f85627201c24fc196fba53f5786130ebc82b3a
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-13 23:34:00 +0500

    v.004

--- src/components/submarine.js
+++ src/components/submarine.js
@@ -3,25 +3,29 @@ import './style2.css';
 
 const Submarine = () => {
   return (
+
     <div className="sea">
-        <div className="circle-wrapper">
-            <div className="bubble"></div>
-            <div className="submarine-wrapper">
-                <div className="submarine-body">
-                    <div className="window"></div>
-                    <div className="engine"></div>
-                    <div className="light"></div>
-                </div>
-                <div className="helix"></div>
-                <div className="hat">
-                  <div className="leds-wrapper">
-                      <div className="periscope"></div>
-                      <div className="leds"></div>
-                  </div>
-                </div>
-            </div>
-        </div>
-    </div>
+     <div className="scale-wrapper">
+       <div className="circle-wrapper">
+         <div className="bubble"></div>
+         <div className="submarine-wrapper">
+           <div className="submarine-body">
+             <div className="window"></div>
+             <div className="engine"></div>
+             <div className="light"></div>
+           </div>
+           <div className="helix"></div>
+           <div className="hat">
+             <div className="leds-wrapper">
+               <div className="periscope"></div>
+               <div className="leds"></div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+
   );
 }
 
