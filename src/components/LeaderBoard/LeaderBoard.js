3fb3f498c5726b331e10823dbbc2d76960f166f6
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-14 19:29:55 +0500

    with name tg

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './LeaderBoard.css';
 import logo from '../images/logo.png';
 import medal from '../images/medal-bronze.png';
 
 const LeaderBoard = () => {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    // Check if Telegram WebApp is available
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+      setUserName(tg.initDataUnsafe.user.first_name || 'Guest');
+    }
+  }, []);
+
   return (
     <div className="leader-board">
       <div className="leader-board-title">
         <div>
-        <img className="smallLogo" src={logo} alt={'sa'} />
+          <img className="smallLogo" src={logo} alt={'Logo'} />
         </div>
         <div className='name-section'>
-          <h3 className='name'>Ilya</h3>
+          <h3 className='name'>{userName}</h3>
           <a className='link-leaderboard' href="URL-ссылки">LeaderBoard</a>
         </div>
       </div>
       <div className="leader-board-awards">
-      <div className='medal-section'>
-      <img className="smallLogo-medal" src={medal} alt={'sa'} />
-      <a className='link-medal' href="URL-ссылки">Ranks</a>
-      </div>
+        <div className='medal-section'>
+          <img className="smallLogo-medal" src={medal} alt={'Medal'} />
+          <a className='link-medal' href="URL-ссылки">Ranks</a>
+        </div>
       </div>
     </div>
   );
