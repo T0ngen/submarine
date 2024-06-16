92a8aa49fdbe774e988a6c128a9267aba202f27c
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-16 14:46:15 +0500

    with bottom menu

--- src/components/menuButtons/menuButtons.js
+++ src/components/menuButtons/menuButtons.js
@@ -1,12 +1,17 @@
 import './menuButtons.css';
+import React from 'react';
+
+
 
 const MenuButtons = () => {
-
     return (
         <div className="menu-block">
-         
+            <button className="menu-button">Заработать</button>
+            <button className="menu-button">Улучшения</button>
+            <button className="menu-button">Улучшения</button>
+            <button className="menu-button">Друзья</button>
         </div>
-      ); 
+    );
 };
 
 export default MenuButtons;
