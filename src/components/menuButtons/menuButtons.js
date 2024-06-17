fcad9e94e4e92c4666fe34cfee5d137df32b8150
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-17 18:04:06 +0500

    add buttons

--- src/components/menuButtons/menuButtons.js
+++ src/components/menuButtons/menuButtons.js
@@ -1,15 +1,36 @@
 import './menuButtons.css';
 import React from 'react';
-
-
-
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import UnarchiveIcon from '@mui/icons-material/Unarchive';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 const MenuButtons = () => {
     return (
         <div className="menu-block">
-            <div className="menu-button">Заработать</div>
-            <div className="menu-button">Улучшения</div>
-            <div className="menu-button">Улучшения</div>
-            <div className="menu-button">Друзья</div>
+            <div className="menu-button">
+                <div className="menu-button-content">
+                    <CurrencyExchangeIcon color="disabled" />
+                    <p className='text-buttons'>Заработать</p>
+                </div>
+            </div>
+            <div className="menu-button">
+                <div className="menu-button-content">
+                <UnarchiveIcon color="disabled" />
+                <p className='text-buttons'>Улучшения</p>
+                </div>
+            </div>
+            <div className="menu-button">
+                <div className="menu-button-content">
+                <AssignmentIcon color="disabled" />
+                <p className='text-buttons'>Задания</p>
+                </div>
+            </div>
+            <div className="menu-button">
+                <div className="menu-button-content">
+                <PeopleAltIcon color="disabled" />
+                <p className='text-buttons'>Друзья</p>
+                </div>
+            </div>
         </div>
     );
 };
